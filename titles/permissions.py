01fa18c19f71d76a50c5aa9514aa7ba2af180b13
Артемий Спесивцев <temana2556@yandex.ru>
2021-03-16 15:45:33 +0300

    third_commit

--- titles/permissions.py
+++ titles/permissions.py
@@ -3,4 +3,5 @@ from rest_framework import permissions
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS) or (request.user.is_authenticated and request.user.is_admin())
+        return ((request.method in permissions.SAFE_METHODS)
+                or (request.user.is_authenticated and request.user.is_admin()))
